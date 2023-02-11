ea13a88401c961baccc10e73620c3422ee0e6b65
maiquegouveia <maiquebusiness@gmail.com>
2023-02-11 17:35:07 -0300

    Weather app

---
+++ app.py
@@ -0,0 +1,42 @@
+import requests
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = r'sqlite:///C:\Users\Maique\Desktop\social_network\weather.db'
+db = SQLAlchemy(app)
+
+
+class City(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+       new_city = request.form.get('city')
+       if new_city:
+        db.session.add(City(name=new_city))
+        db.session.commit()
+       
+    cities = City.query.order_by(City.id.desc())
+    url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=698693a3102e75a22db495db1736fbc1'
+
+    weather_data = []
+
+    for city in cities:
+        r = requests.get(url.format(city.name)).json()
+
+        weather = {
+            'city': r['name'],
+            'temp': round(r['main']['temp']),
+            'desc': r['weather'][0]['description'],
+            'icon': r['weather'][0]['icon'],
+        }
+        weather_data.append(weather)
+    return render_template('weather.html', weather_data=weather_data)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
